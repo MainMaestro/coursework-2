5a3d4ce0c5448aff80af744a769fe3e228a7f8fb
Армен <armen.sargsyan2003@mail.ru>
2025-06-22 17:33:45 +0300

    инициализация проекта через nuxt

--- src/pages/Header.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <header class="bg-white shadow rounded-xl p-4 text-center">
-    <h1 class="text-2xl font-bold text-blue-800">Автоматизация учебного расписания</h1>
-    <p class="text-gray-600 text-sm">Весенний семестр 2024–2025</p>
-  </header>
-</template>
--- src/pages/SideBar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <aside class="bg-white rounded-xl shadow p-4 space-y-3">
-    <RouterLink to="/groups" class="block text-blue-600 hover:underline">Группы</RouterLink>
-    <RouterLink to="/teachers" class="block text-blue-600 hover:underline">Преподаватели</RouterLink>
-    <RouterLink to="/classrooms" class="block text-blue-600 hover:underline">Аудитории</RouterLink>
-    <RouterLink to="/schedule" class="block text-blue-600 hover:underline">Расписание</RouterLink>
-  </aside>
-</template>
